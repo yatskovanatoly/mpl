91d086bd0b943228ef57fc1290ae2e49dec25eb2
Anatoly <yatskovanatoly@gmail.com>
2025-04-25 15:23:52 +0300

    Minor styling

--- components/Round.tsx
+++ components/Round.tsx
@@ -1,18 +1,18 @@
-import Games from '@/components/Games'
-import RoundSelect from '@/components/RoundSelect'
-import getData, { getRounds } from '@/lib/harvest-data'
-import { Game } from '@/lib/types'
+import Games from "@/components/Games";
+import RoundSelect from "@/components/RoundSelect";
+import getData, { getRounds } from "@/lib/harvest-data";
+import { Game } from "@/lib/types";
 
 export default async function GamesPage({ round }: { round?: number }) {
-	const data: Game[] = await getData(round)
-	const { rounds, currentRound } = await getRounds(round)
+  const data: Game[] = await getData(round);
+  const { rounds, currentRound } = await getRounds(round);
 
-	return (
-		<div className='flex flex-col items-center'>
-			<div>
-				Раунд: <RoundSelect rounds={rounds} currentRound={currentRound} />
-			</div>
-			<Games games={data} />
-		</div>
-	)
+  return (
+    <div className="flex flex-col items-center gap-4">
+      <div>
+        Раунд: <RoundSelect rounds={rounds} currentRound={currentRound} />
+      </div>
+      <Games games={data} />
+    </div>
+  );
 }
