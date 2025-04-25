05618563ca74c7188ffd8658fd8f8966b5655ff8
Anatoly <yatskovanatoly@gmail.com>
2025-04-25 16:45:55 +0300

    Export build

--- app/games/[round]/page.tsx
+++ app/games/[round]/page.tsx
@@ -1,6 +1,20 @@
-import GamesPage from '@/components/Round'
+import GamesPage from "@/components/Round";
+import { getRounds } from "@/lib/harvest-data";
 
-export default function Page({ params }: { params: { round: string } }) {
-	const round = Number(params.round)
-	return <GamesPage round={isNaN(round) ? undefined : round} />
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ round: string }>;
+}) {
+  const round = await params;
+  const roundNum = Number(round);
+  return <GamesPage round={roundNum} />;
+}
+
+export async function generateStaticParams() {
+  const { rounds } = await getRounds();
+
+  return rounds.map((round) => ({
+    round: String(round),
+  }));
 }
--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
-import type { NextConfig } from 'next'
-import { BASE_URL } from './lib/urls'
+import type { NextConfig } from "next";
+import { BASE_URL } from "./lib/urls";
 
 const nextConfig: NextConfig = {
-	images: {
-		remotePatterns: [new URL(`${BASE_URL}/**`)],
-	},
-}
+  output: "export",
+  basePath: "/mpl-results",
+  assetPrefix: "/mpl-resutls",
+  images: {
+    remotePatterns: [new URL(`${BASE_URL}/**`)],
+    unoptimized: true,
+  },
+};
 
-export default nextConfig
+export default nextConfig;
