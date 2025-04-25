5a0ffab39209c7e676ad0c4c581ad13826154f05
Anatoly <yatskovanatoly@gmail.com>
2025-04-25 17:10:44 +0300

    Deploy file

--- app/games/[round]/page.tsx
+++ app/games/[round]/page.tsx
@@ -1,20 +1,21 @@
-import GamesPage from "@/components/Round";
-import { getRounds } from "@/lib/harvest-data";
+import GamesPage from "@/components/Round"
+import { getRounds } from "@/lib/harvest-data"
 
 export default async function Page({
   params,
 }: {
-  params: Promise<{ round: string }>;
+  params: Promise<{ round: string }>
 }) {
-  const round = await params;
-  const roundNum = Number(round);
-  return <GamesPage round={roundNum} />;
+  const round = await params
+  const roundNum = Number(round)
+  
+  return <GamesPage round={roundNum} />
 }
 
 export async function generateStaticParams() {
-  const { rounds } = await getRounds();
+  const { rounds } = await getRounds()
 
   return rounds.map((round) => ({
     round: String(round),
-  }));
+  }))
 }
