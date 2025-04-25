1d1ee6a6acc2e8c8d8a5fa1139fdcf30261799f4
Anatoly <yatskovanatoly@gmail.com>
2025-04-25 17:12:31 +0300

    Upd next.config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,12 @@
-import type { NextConfig } from "next";
-import { BASE_URL } from "./lib/urls";
+import type { NextConfig } from "next"
+import { BASE_URL } from "./lib/urls"
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/mpl-results",
-  assetPrefix: "/mpl-resutls",
   images: {
     remotePatterns: [new URL(`${BASE_URL}/**`)],
     unoptimized: true,
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
