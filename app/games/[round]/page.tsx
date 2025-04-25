544555d8736dd73f4f2d97f54e69c142557dcdd0
Anatoly <yatskovanatoly@gmail.com>
2025-04-25 17:36:22 +0300

    Rm attempt to static

--- app/games/[round]/page.tsx
+++ app/games/[round]/page.tsx
@@ -1,5 +1,4 @@
 import GamesPage from "@/components/Round"
-import { getRounds } from "@/lib/harvest-data"
 
 export default async function Page({
   params,
@@ -8,14 +7,6 @@ export default async function Page({
 }) {
   const round = await params
   const roundNum = Number(round)
-  
+
   return <GamesPage round={roundNum} />
-}
-
-export async function generateStaticParams() {
-  const { rounds } = await getRounds()
-
-  return rounds.map((round) => ({
-    round: String(round),
-  }))
 }
--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,8 @@ import type { NextConfig } from "next"
 import { BASE_URL } from "./lib/urls"
 
 const nextConfig: NextConfig = {
-  output: "export",
   images: {
     remotePatterns: [new URL(`${BASE_URL}/**`)],
-    unoptimized: true,
   },
 }
 
