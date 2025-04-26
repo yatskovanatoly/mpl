2d1cf4a6a36aa72926af7dd9f7b841bba14d2dac
Anatoly <yatskovanatoly@gmail.com>
2025-04-26 15:03:16 +0300

    HandleClick, styles for mobile

--- components/RoundSelect.tsx
+++ components/RoundSelect.tsx
@@ -2,13 +2,14 @@
 
 import { Rounds } from "@/lib/types"
 import { useParams, useRouter } from "next/navigation"
-import { ChangeEvent, FC, useState } from "react"
+import { ChangeEvent, FC, RefObject, useRef, useState } from "react"
 
 const RoundSelect: FC<Rounds> = ({ rounds, currentRound }) => {
   const params = useParams<{ round: string }>()
   const initialRound = params.round ?? currentRound
   const [loading, setLoading] = useState(false)
   const router = useRouter()
+  const ref = useRef<HTMLSelectElement>(null)
 
   const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
     const value = event.target.value
@@ -25,11 +26,12 @@ const RoundSelect: FC<Rounds> = ({ rounds, currentRound }) => {
 
   return (
     <div
+      onClick={() => handleClick(ref)}
       aria-disabled={loading}
-      className={`${loading && "muted pointer-events-none"}`}
+      className={`${loading && "muted pointer-events-none"} flex cursor-default items-center gap-1 max-sm:border-b`}
     >
-      Раунд:
-      <select defaultValue={initialRound} onChange={handleChange}>
+      <div>Раунд:</div>
+      <select ref={ref} defaultValue={initialRound} onChange={handleChange}>
         {rounds.map((round, i) => (
           <option key={i + round} value={round}>
             {round}
@@ -38,6 +40,21 @@ const RoundSelect: FC<Rounds> = ({ rounds, currentRound }) => {
       </select>
     </div>
   )
+}
+
+const handleClick = (ref: RefObject<HTMLSelectElement | null>) => {
+  if (!ref || !ref.current) return
+
+  try {
+    if (ref.current.showPicker) {
+      ref.current.showPicker()
+    } else {
+      ref.current.focus()
+    }
+  } catch (e) {
+    ref.current.focus()
+    console.error(e)
+  }
 }
 
 export default RoundSelect
