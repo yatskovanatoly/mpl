d66bb33f1967d9eaed9a9939938f6263fcf2ec64
Anatoly <yatskovanatoly@gmail.com>
2025-04-26 13:32:54 +0300

    Fira font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import ThemeToggle from "@/components/ThemeToggle"
 import type { Metadata } from "next"
 import { ThemeProvider } from "next-themes"
-import { Geist, Geist_Mono } from "next/font/google"
+import { Fira_Sans } from "next/font/google"
 import "./globals.css"
 
 export default function RootLayout({
@@ -11,12 +11,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${fira.variable} antialiased`}>
         <ThemeProvider attribute="class" enableSystem defaultTheme="system">
-          <div className="flex min-h-dvh flex-col items-center justify-center p-6 font-[family-name:var(--font-geist-sans)]">
-            <div className="fixed top-2 right-4">
+          <div className="font-fira-sans flex min-h-dvh flex-col items-center justify-center p-6">
+            <div className="fixed top-4 right-6">
               <ThemeToggle />
             </div>
             {children}
@@ -27,14 +25,10 @@ export default function RootLayout({
   )
 }
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-})
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const fira = Fira_Sans({
+  subsets: ["cyrillic"],
+  variable: "--font-fira-sans",
+  weight: "500",
 })
 
 export const metadata: Metadata = {
