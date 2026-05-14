d060e3049cadfb5e99d24a5821b07e9fae8e9335
Anatoly <yatskovanatoly@gmail.com>
2026-05-14 12:55:32 +0300

    mpl logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,9 @@ import ThemeToggle from "@/components/ThemeToggle"
 import type { Metadata } from "next"
 import { ThemeProvider } from "next-themes"
 import { Roboto } from "next/font/google"
+import Link from "next/link"
 import "./globals.css"
+import Image from "next/image"
 
 export default function RootLayout({
   children,
@@ -14,6 +16,18 @@ export default function RootLayout({
       <body className={`${roboto.className} antialiased`}>
         <ThemeProvider attribute="class" enableSystem defaultTheme="system">
           <div className="flex min-h-dvh flex-col items-center justify-center p-4">
+            <Link
+              href="/games"
+              aria-label="Moscow Punk-rock League games"
+              className="fixed top-2 left-2 size-10 sm:top-4 sm:left-4 sm:size-12"
+            >
+              <Image
+                src="/mpl.png"
+                alt="Moscow Punk-rock League"
+                width={100}
+                height={100}
+              />
+            </Link>
             <div className="fixed top-4 right-6">
               <ThemeToggle />
             </div>
